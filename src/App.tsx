5907e88fd296c7cf1b73dccdff03167698325f60
cipriandraghici <cipriandraghici93@gmail.com>
2023-10-02 19:46:34 +0300

    login logout complete flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import './App.css'
 import {DappProvider} from "@multiversx/sdk-dapp/wrappers";
 import {ENVIRONMENT} from "config";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import {HomePage} from "pages/HomePage";
-import {UnlockPage} from "pages/UnlockPage";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HomePage} from "pages/HomePage.tsx";
+import {UnlockPage} from "pages/UnlockPage.tsx";
 
 function App() {
 
-  return (
-      <DappProvider environment={ENVIRONMENT}>
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/unlock" element={<UnlockPage />} />
-            </Routes>
-        </Router>
-      </DappProvider>
-  )
+    return (
+        <DappProvider environment={ENVIRONMENT}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/unlock" element={<UnlockPage />} />
+                </Routes>
+            </Router>
+        </DappProvider>
+    )
 }
 
 export default App
--- src/pages/UnlockPage.tsx
+++ src/pages/UnlockPage.tsx
@@ -1,7 +1,32 @@
+import {
+    ExtensionLoginButton,
+    WebWalletLoginButton,
+    LedgerLoginButton,
+    WalletConnectLoginButton
+} from "@multiversx/sdk-dapp/UI";
+
 export const UnlockPage = () => {
     return (
         <div>
-            <h1>Unlock Page</h1>
+            <h2>Login with:</h2>
+            <div>
+                <ExtensionLoginButton
+                    loginButtonText="DeFi Wallet"
+                    callbackRoute="/"
+                />
+                <WebWalletLoginButton
+                    loginButtonText={"Web Wallet"}
+                    callbackRoute="/"
+                />
+                <LedgerLoginButton
+                    loginButtonText='Ledger'
+                    callbackRoute="/"
+                />
+                <WalletConnectLoginButton
+                    loginButtonText='xPortal App'
+                    callbackRoute="/"
+                />
+            </div>
         </div>
     )
 }
