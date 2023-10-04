250d3815cbefaa7163a82d4d4e36a7445f99f36a
cipriandraghici <cipriandraghici93@gmail.com>
2023-10-04 15:01:33 +0300

    add styles for create offer page

--- src/pages/CreateOfferPage/CreateOfferPage.tsx
+++ src/pages/CreateOfferPage/CreateOfferPage.tsx
@@ -1,13 +1,22 @@
+import { useGetIsLoggedIn } from "@multiversx/sdk-dapp/hooks/account/useGetIsLoggedIn";
 import {CreateOfferFormik} from "./components/CreateOfferFormik";
 import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 export const CreateOfferPage = () => {
     const navigate = useNavigate();
+    const isLoggedIn = useGetIsLoggedIn();
+
+    useEffect(() => {
+        if(!isLoggedIn) {
+            navigate("/unlock");
+        }
+    });
 
     return (
         <div>
             <div>
-                <button onClick={() => navigate(-1)}>{`< Back`}</button>
+                <button style={{display: "flex"}} onClick={() => navigate(-1)}>{`< Back`}</button>
                 <h2>Create Offer</h2>
             </div>
             <CreateOfferFormik />
--- src/pages/CreateOfferPage/components/CreateOfferForm.tsx
+++ src/pages/CreateOfferPage/components/CreateOfferForm.tsx
@@ -5,86 +5,89 @@ export const CreateOfferForm = () => {
     const { errors } = useFormikContext<CreateOfferValuesType>();
 
     return (
-        <Form>
+        <Form className="form">
+            <div className="card" style={{display: "flex", justifyContent: "center", flexDirection: "column"}}>
+                <h4><b>Your NFT:</b></h4>
+                <div>
+                    <label>NFT Collection</label>
+                    <Field
+                        type="text"
+                        placeholder="NFT collection"
+                        id="collectionId"
+                        name="collectionId"
+                    />
+                    <ErrorMessage
+                        name="collectionId"
+                        component="div"
+                    />
+                </div>
+                <div>
+                    <label>NFT Nonce</label>
+                    <Field
+                        type="number"
+                        placeholder="NFT nonce"
+                        id="collectionNonce"
+                        name="collectionNonce"
+                        min="-1"
+                    />
+                    <ErrorMessage
+                        name="collectionNonce"
+                        component="div"
+                    />
+                </div>
 
-            <div>Your NFT:</div>
-            <div>
-                <label>NFT Collection</label>
-                <Field
-                    type="text"
-                    placeholder="NFT collection"
-                    id="collectionId"
-                    name="collectionId"
-                />
-                <ErrorMessage
-                    name="collectionId"
-                    component="div"
-                />
-            </div>
-            <div>
-                <label>NFT Nonce</label>
-                <Field
-                    type="text"
-                    placeholder="NFT nonce"
-                    id="collectionNonce"
-                    name="collectionNonce"
-                />
-                <ErrorMessage
-                    name="collectionNonce"
-                    component="div"
-                />
+                <h4><b>Desired NFT:</b></h4>
+                <div>
+                    <label>NFT Collection</label>
+                    <Field
+                        type="text"
+                        placeholder="Wanted NFT collection"
+                        id="wantedCollectionId"
+                        name="wantedCollectionId"
+                    />
+                    <ErrorMessage
+                        name="wantedCollectionId"
+                        component="div"
+                    />
+                </div>
+                <div>
+                    <label>NFT Nonce</label>
+                    <Field
+                        type="number"
+                        placeholder="Wanted NFT nonce"
+                        id="wantedCollectionNonce"
+                        name="wantedCollectionNonce"
+                    />
+                    <ErrorMessage
+                        name="wantedCollectionNonce"
+                        component="div"
+                    />
+                </div>
+
+                <div>
+                    <label>Wanted address</label>
+                    <Field
+                        type="text"
+                        placeholder="Wanted address"
+                        id="wantedAddress"
+                        name="wantedAddress"
+                    />
+                    <ErrorMessage
+                        name="wantedAddress"
+                        component="div"
+                    />
+                </div>
+
+                <button
+                    type="submit"
+                    disabled={Object.entries(errors).length > 0}
+                >
+                    Submit
+                </button>
             </div>
 
-            <hr />
 
-            <div>Wanted NFT:</div>
-            <div>
-                <label>NFT Collection</label>
-                <Field
-                    type="text"
-                    placeholder="Wanted NFT collection"
-                    id="wantedCollectionId"
-                    name="wantedCollectionId"
-                />
-                <ErrorMessage
-                    name="wantedCollectionId"
-                    component="div"
-                />
-            </div>
-            <div>
-                <label>NFT Nonce</label>
-                <Field
-                    type="text"
-                    placeholder="Wanted NFT nonce"
-                    id="wantedCollectionNonce"
-                    name="wantedCollectionNonce"
-                />
-                <ErrorMessage
-                    name="wantedCollectionNonce"
-                    component="div"
-                />
-            </div>
 
-            <div>
-                <label>Wanted address</label>
-                <Field
-                    type="text"
-                    placeholder="Wanted address"
-                    id="wantedAddress"
-                    name="wantedAddress"
-                />
-                <ErrorMessage
-                    name="wantedAddress"
-                    component="div"
-                />
-            </div>
-
-            <button
-                type="submit"
-                disabled={Object.entries(errors).length > 0}
-            >
-                Submit
-            </button>
         </Form>
     )
 }
